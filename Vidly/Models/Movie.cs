aa321ca23f3965471989ffa44adf1671ded94cc7
Rich Montero <monterorf@gmail.com>
2020-06-07 13:41:08 -0700

    Section 5 complete. I guess.

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -7,13 +7,19 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         [Required]
-        [StringLength(255)]
-        public string Name { get; set; }        
-        public DateTime ReleaseDate { get; set; }        
-        public DateTime DateAdded { get; set; }                
-        public int NumerIsStock { get; set; }
+        [StringLength(255)]    
+        public string Name { get; set; }
         [Required]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }        
+        public DateTime DateAdded { get; set; }
+        [Display(Name = "Number in Stock")]
+        [Required]
+        [Range(1,20,ErrorMessage ="Number in stock must be between 1 and 20")]
+        public int NumerIsStock { get; set; }
         public Genre Genre { get; set; }
+        [Display(Name = "Genre")]
+        [Required]
         public byte GenreId { get; set; }
     }
 }
